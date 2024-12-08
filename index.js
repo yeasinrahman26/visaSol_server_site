5d92af38f2c6077c09b5f83b6fb7573de212c046
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2024-12-08 23:19:59 +0600

    adding visa details page

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const app=express();
-const port =process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express.json())
-
-// visaSol
-//Iamsafa007
-
-// console.log(process.env.DB_USER);
-// console.log(process.env.DB_PASS);
-
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yog8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // const uri =
-//   "mongodb+srv://<db_username>:<db_password>@cluster0.yog8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//   "mongodb+srv://visaSol:Iamsafa007@cluster0.yog8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,6 +27,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const visaCollection = client.db("visaDB").collection("visa");
+    
+
+    app.get("/visas", async (req, res) => {
+      const cursor = visaCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    
+    app.get("/visas/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await visaCollection.findOne(query);
+        res.send(result);
+    });
+    
+    app.post("/visas", async (req, res) => {
+        const newVisa = req.body;
+        console.log(newVisa);
+        const result = await visaCollection.insertOne(newVisa);
+        res.send(result);
+    });
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -42,25 +60,14 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('visa sol is running')
-})
-app.listen(port,()=>{
-    console.log(`visa sol is running on port${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("visa sol is running");
+});
+app.listen(port, () => {
+  console.log(`visa sol is running on port${port}`);
+});
